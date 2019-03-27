013416cc9e4e258512644c8204cf322d41430241
David Stewart <davidstewart@gmail.com>
2019-03-27 02:23:52 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,18 @@
 class Person
-
-
+  def person=(name)
+    @person = name
+  end
+  
+  def person
+    @name
+  end
+  def job=(job)
+    @job = job
+  end
+  
+  def job
+    
+  @job
+end
   
 end
